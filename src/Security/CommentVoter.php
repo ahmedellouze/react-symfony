597721a6568078865ee8dab2c2a42ede0df3f3c3
ahmed.ellouze <ahmed.ellouze@sifast.com>
2022-04-11 10:02:25 +0200

    correction

--- src/Security/CommentVoter.php
+++ src/Security/CommentVoter.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Security;
-
 
 use App\Entity\Comment;
 use App\Entity\User;
@@ -10,25 +8,23 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\The;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
-
 class CommentVoter extends Voter
 {
 
-const EDIT='EDIT_COMMENT';
+    public const EDIT = 'EDIT_COMMENT';
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return $attribute===self::EDIT && $subject instanceof Comment;
-
+        return self::EDIT === $attribute && $subject instanceof Comment;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
-        $user= $token->getUser();
-        if(!$user instanceof User ||!$subject instanceof Comment){
+        $user = $token->getUser();
+        if (!$user instanceof User || !$subject instanceof Comment) {
             return false;
         }
-        
+
         return $subject->getAuthor()->getId() === $user->getId();
     }
 }
